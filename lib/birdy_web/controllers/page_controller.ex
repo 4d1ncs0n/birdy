dfcfdacce1f9609d550c50c5de8d1f9354e189a7
Danny Velasquez <adincson@gmail.com>
2021-11-18 00:24:08 -0500

    feat: changeset pipeline to signup created

--- lib/birdy_web/controllers/page_controller.ex
+++ lib/birdy_web/controllers/page_controller.ex
@@ -1,15 +1,18 @@
 defmodule BirdyWeb.PageController do
   use BirdyWeb, :controller
 
+  alias Birdy.Accounts
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    render conn
   end
 
   def signup(conn, _params) do
-    render(conn, "signup.html")
+    changeset = Accounts.register_changeset()
+    render conn, user_chainset: changeset
   end
 
   def login(conn, _params) do
-    render(conn, "login.html")
+    render conn
   end
 end
