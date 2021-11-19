4c76121f03706f946509a1c8e22a19ee82d14b06
Danny Velasquez <adincson@gmail.com>
2021-11-18 19:24:21 -0500

    feat: post method used for new user and changeset

--- lib/birdy_web/controllers/page_controller.ex
+++ lib/birdy_web/controllers/page_controller.ex
@@ -9,10 +9,25 @@ defmodule BirdyWeb.PageController do
 
   def signup(conn, _params) do
     changeset = Accounts.register_changeset()
-    render conn, user_chainset: changeset
+    render conn, user_changeset: changeset
   end
 
   def login(conn, _params) do
     render conn
+  end
+
+  def create_user(conn, %{"user" => user_params}) do
+    case Accounts.create_user(user_params) do
+      {:ok, _user} ->
+        conn
+        |> put_flash(:info, "Usuario creado satisfactoriamente!")
+        |> redirect(to: "/")
+      {:error, user_changeset} ->
+        IO.inspect user_changeset
+
+        conn
+        |> put_flash(:error, "No se pudo crear la cuenta")
+        |> render("signup.html", user_changeset: user_changeset)
+    end
   end
 end
